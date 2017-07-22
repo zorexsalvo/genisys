b75ad4491c636f241980f43c3db097cc95acb1b2
Zorex Salvo <zorexsalvo@gmail.com>
2017-07-22 16:57:35 +0800

    Add open button to your paluwagan

--- mobile/urls.py
+++ mobile/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^ogp/$', OGP.as_view(), name='ogp'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^paluwagan/$', login_required(MyPaluwagan.as_view()), name='paluwagan'),
     url(r'^oauth/', include('social_django.urls'), name='social'),
     url(r'^$', login_required(HomeView.as_view()), name='home'),
     url(r'^myaccount/$', login_required(MyPage.as_view()), name='mypage'),
